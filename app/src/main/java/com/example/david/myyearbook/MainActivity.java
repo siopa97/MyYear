948acd815fd14713dcd0888bab3a36e8d2bd4378
David Siopa <siopa97@hotmail.com>
2018-03-07 18:20:15 -0500

    updates

--- app/src/main/java/com/example/david/myyearbook/MainActivity.java
+++ app/src/main/java/com/example/david/myyearbook/MainActivity.java
@@ -1,20 +1,34 @@
 package com.example.david.myyearbook;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.weatherbtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                weather(v);
+            }
+        });
     }
 
     public void resume(View view) {
         Intent i = new Intent(MainActivity.this, Resume.class);
+        startActivity(i);
+    }
+
+    public void weather(View view) {
+        Intent i = new Intent(MainActivity.this, Weather.class);
         startActivity(i);
     }
 
---
+++ app/src/main/java/com/example/david/myyearbook/Weather.java
@@ -0,0 +1,18 @@
+package com.example.david.myyearbook;
+
+import android.net.Network;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.net.URL;
+
+public class Weather extends AppCompatActivity{
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.weather);
+        }
+        
+}
