f88d889e772740e04b8c6010f91692cff791d6d9
David Siopa <siopa97@hotmail.com>
2018-03-12 23:14:33 -0400

    updates

--- app/src/main/java/com/example/david/myyearbook/Weather.java
+++ app/src/main/java/com/example/david/myyearbook/Weather.java
@@ -1,5 +1,6 @@
 package com.example.david.myyearbook;
 
+import android.content.Intent;
 import android.net.Network;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -19,6 +20,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -32,7 +34,7 @@ public class Weather extends AppCompatActivity {
     WeatherList data = new WeatherList();
     Button update;
 
-    private String path = "http://api.weatherunlocked.com/api/forecast/40.71,-74.00?app_id=b51fbba4&app_key=7a45bd08651c576aeef6b08b1b2bb5f0";
+    private String path = "http://api.weatherunlocked.com/api/forecast/40.71,74.00?app_id=b51fbba4&app_key=7a45bd08651c576aeef6b08b1b2bb5f0";
 
     public Weather() throws MalformedURLException {
     }
@@ -42,97 +44,28 @@ public class Weather extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather);
 
-
         new getWeather().execute();
 
-        update = findViewById(R.id.update);
-        update.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fill();
-            }
-        });
+    }
+
+    public void refresh() {
+        //Intent i = new Intent(Weather.this, Weather.class);
+        //fill();
+        startActivity(getIntent());
+        finish();
 
 
     }
 
-    public void fill() {
-        TextView remove = findViewById(R.id.remove);
-        remove.setHeight(0);
 
-        //DAY 1
-        TextView date1 = findViewById(R.id.date1);
-        TextView min1 = findViewById(R.id.minTemp1);
-        TextView max1 = findViewById(R.id.maxTemp1);
-        TextView snow1 = findViewById(R.id.snow1);
-        TextView rain1 = findViewById(R.id.chanceofRain1);
-
-        date1.setText(data.get(0).getDate());
-        min1.setText(data.get(0).getTempMin());
-        max1.setText(data.get(0).getTempMax());
-        snow1.setText(data.get(0).getSnow());
-        rain1.setText(data.get(0).getRainPercent() + "");
-
-        //DAY2
-        TextView date2 = findViewById(R.id.date2);
-        TextView min2 = findViewById(R.id.minTemp2);
-        TextView max2 = findViewById(R.id.maxTemp2);
-        TextView snow2 = findViewById(R.id.snow2);
-        TextView rain2 = findViewById(R.id.chanceofRain2);
-
-        date2.setText(data.get(1).getDate());
-        min2.setText(data.get(1).getTempMin());
-        max2.setText(data.get(1).getTempMax());
-        snow2.setText(data.get(1).getSnow());
-        rain2.setText(data.get(1).getRainPercent() + "");
-
-        //DAY3
-        TextView date3 = findViewById(R.id.date3);
-        TextView min3 = findViewById(R.id.minTemp3);
-        TextView max3 = findViewById(R.id.maxTemp3);
-        TextView snow3 = findViewById(R.id.snow3);
-        TextView rain3 = findViewById(R.id.chanceofRain3);
-
-        date3.setText(data.get(2).getDate());
-        min3.setText(data.get(2).getTempMin());
-        max3.setText(data.get(2).getTempMax());
-        snow3.setText(data.get(2).getSnow());
-        rain3.setText(data.get(2).getRainPercent() + "");
-
-        //DAY 4
-        TextView date4 = findViewById(R.id.date4);
-        TextView min4 = findViewById(R.id.minTemp4);
-        TextView max4 = findViewById(R.id.maxTemp4);
-        TextView snow4 = findViewById(R.id.snow4);
-        TextView rain4 = findViewById(R.id.chanceofRain4);
-
-        date4.setText(data.get(3).getDate());
-        min4.setText(data.get(3).getTempMin());
-        max4.setText(data.get(3).getTempMax());
-        snow4.setText(data.get(3).getSnow());
-        rain4.setText(data.get(3).getRainPercent() + "");
-
-        //DAY5
-        TextView date5 = findViewById(R.id.date5);
-        TextView min5 = findViewById(R.id.minTemp5);
-        TextView max5 = findViewById(R.id.maxTemp5);
-        TextView snow5 = findViewById(R.id.snow5);
-        TextView rain5 = findViewById(R.id.chanceofRain5);
-
-        date5.setText(data.get(4).getDate());
-        min5.setText(data.get(4).getTempMin());
-        max5.setText(data.get(4).getTempMax());
-        snow5.setText(data.get(4).getSnow());
-        rain5.setText(data.get(4).getRainPercent() + "");
-    }
-
-
-    class getWeather extends AsyncTask {
+    private class getWeather extends AsyncTask<Void, Void, String> {
 
 
         @Override
-        protected Object doInBackground(Object[] objects) {
-            return getWebServiceData();
+        protected String doInBackground(Void... String) {
+            String hello = "";
+            getWebServiceData();
+            return hello;
         }
 
         protected Void getWebServiceData() {
@@ -176,11 +109,10 @@ public class Weather extends AppCompatActivity {
                     Log.d("Max: ", "" + max);
                     double min = jsonobject.getInt("temp_min_f");
                     Log.d("Min: ", "" + min);
-                    double snow = jsonobject.getInt("snow_total_in");
                     int chance = jsonobject.getInt("prob_precip_pct");
                     Log.d("Weather Report:", date + " minTemp:" + min + " maxTemp:" + max +
-                            " snowIn:" + snow + " Chance of Rain: " + chance);
-                    weather = new WeatherObject(date, max, min, snow, chance);
+                            " Chance of Rain: " + chance);
+                    weather = new WeatherObject(date, max, min, chance);
                     data.add(weather);
 
                 }
@@ -191,7 +123,74 @@ public class Weather extends AppCompatActivity {
             return null;
         }
 
+        @Override
+        protected void onPostExecute(String hello) {
+            try {
+                fill();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
 
+
+    }
+
+    public void fill() throws ParseException {
+
+        //DAY 1
+        TextView date1 = findViewById(R.id.date1);
+        TextView min1 = findViewById(R.id.minTemp1);
+        TextView max1 = findViewById(R.id.maxTemp1);
+        TextView rain1 = findViewById(R.id.chanceofRain1);
+
+        date1.setText(data.get(0).getDate());
+        min1.setText(data.get(0).getTempMin());
+        max1.setText(data.get(0).getTempMax());
+        rain1.setText(data.get(0).getRainPercent() + "");
+
+        //DAY2
+        TextView date2 = findViewById(R.id.date2);
+        TextView min2 = findViewById(R.id.minTemp2);
+        TextView max2 = findViewById(R.id.maxTemp2);
+        TextView rain2 = findViewById(R.id.chanceofRain2);
+
+        date2.setText(data.get(1).getDate());
+        min2.setText(data.get(1).getTempMin());
+        max2.setText(data.get(1).getTempMax());
+        rain2.setText(data.get(1).getRainPercent() + "");
+
+        //DAY3
+        TextView date3 = findViewById(R.id.date3);
+        TextView min3 = findViewById(R.id.minTemp3);
+        TextView max3 = findViewById(R.id.maxTemp3);
+        TextView rain3 = findViewById(R.id.chanceofRain3);
+
+        date3.setText(data.get(2).getDate());
+        min3.setText(data.get(2).getTempMin());
+        max3.setText(data.get(2).getTempMax());
+        rain3.setText(data.get(2).getRainPercent() + "");
+
+        //DAY 4
+        TextView date4 = findViewById(R.id.date4);
+        TextView min4 = findViewById(R.id.minTemp4);
+        TextView max4 = findViewById(R.id.maxTemp4);
+        TextView rain4 = findViewById(R.id.chanceofRain4);
+
+        date4.setText(data.get(3).getDate());
+        min4.setText(data.get(3).getTempMin());
+        max4.setText(data.get(3).getTempMax());
+        rain4.setText(data.get(3).getRainPercent() + "");
+
+        //DAY5
+        TextView date5 = findViewById(R.id.date5);
+        TextView min5 = findViewById(R.id.minTemp5);
+        TextView max5 = findViewById(R.id.maxTemp5);
+        TextView rain5 = findViewById(R.id.chanceofRain5);
+
+        date5.setText(data.get(4).getDate());
+        min5.setText(data.get(4).getTempMin());
+        max5.setText(data.get(4).getTempMax());
+        rain5.setText(data.get(4).getRainPercent() + "");
     }
 
 }
--- app/src/main/java/com/example/david/myyearbook/WeatherObject.java
+++ app/src/main/java/com/example/david/myyearbook/WeatherObject.java
@@ -1,40 +1,56 @@
 package com.example.david.myyearbook;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class WeatherObject {
     private String date;
     private double tempMax;
     private double tempMin;
-    private double snow;
     private int rainPercent;
 
-    public WeatherObject(String date, double tempMax, double tempMin, double snow, int rainPercent) {
+    public WeatherObject(String date, double tempMax, double tempMin, int rainPercent) {
         this.date = date;
         this.tempMax = tempMax;
         this.tempMin = tempMin;
-        this.snow = snow;
         this.rainPercent = rainPercent;
     }
 
-    public String getDate() {
-        return date;
+    public String getDate() throws ParseException {
+        String newDate = "";
+
+        try {
+            SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+            Date usDate = format.parse(date);
+
+            SimpleDateFormat usFormat = new SimpleDateFormat("MM/dd/yyyy");
+            TimeZone tz = TimeZone.getTimeZone("America/New_York");
+            usFormat.setTimeZone(tz);
+
+            newDate = usFormat.format(usDate);
+
+        } catch(Exception bug){
+            bug.printStackTrace();
+        }
+
+
+        return newDate;
     }
 
     public String getTempMax() {
-        return tempMax + " °F";
-    }
 
-    public String getSnow() {
-        return snow + " inches";
+        return String.valueOf(tempMax) + " °F";
     }
 
     public String getRainPercent() {
-        return rainPercent + "%";
+
+        return String.valueOf(rainPercent) + "%";
     }
 
     public String getTempMin() {
 
-        return tempMin + " °F";
+        return String.valueOf(tempMin) + " °F";
     }
 }
